2e9d6d7dfb0d4f62f8d8208033ee8acacf0b6eb8
Allen Antony <allenantony26@gmail.com>
2024-04-22 16:45:04 +0100

    Added property table

--- models.py
+++ models.py
@@ -10,3 +10,14 @@ class User(db.Model):
 
     def __repr__(self):
         return f"User('{self.username}', '{self.email}')"
+
+class Property(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100), nullable=False)
+    description = db.Column(db.Text, nullable=False)
+    location = db.Column(db.String(100), nullable=False)
+    price = db.Column(db.Float, nullable=False)
+    available = db.Column(db.Boolean, default=True)
+
+    def __repr__(self):
+        return f"Property('{self.name}', '{self.location}', '{self.price}')"

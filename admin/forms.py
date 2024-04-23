33bda9075f3e671a970718f4381bdeefb25b1e6e
Allen Antony <allenantony26@gmail.com>
2024-04-23 01:17:17 +0100

    Removed the admin

--- admin/forms.py
+++
@@ -1,9 +0,0 @@
-from flask_wtf import FlaskForm
-from wtforms import StringField, IntegerField, SubmitField
-from wtforms.validators import DataRequired, NumberRange
-
-class PropertyForm(FlaskForm):
-    name = StringField('Name', validators=[DataRequired()])
-    location = StringField('Location', validators=[DataRequired()])
-    price = IntegerField('Price', validators=[DataRequired(), NumberRange(min=0)])
-    submit = SubmitField('Submit')
